4c95640138d1666fcb87ea251e507c9be5273c7e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-24 09:09:55 -0700

    finish book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,7 +7,25 @@ RSpec.describe Book do
     title: "To Kill a Mockingbird", 
     publication_date: "July 11, 1960"})}
 
-  it 'exists' do 
+  describe "#Book attributes" do
+
+    it 'exists' do 
+      expect(book).to be_a Book
+    end
+
+    it 'the book has a title' do
+      expect(book.title).to eq("To Kill a Mockingbird")
+    end
+
+    it 'the book has an author' do
+      expect(book.author).to eq("Harper Lee")
+    end
+
+    it 'the book has a publication year' do
+      expect(book.publication_year).to eq("1960")
+    end
+  end
+
 
 
 
