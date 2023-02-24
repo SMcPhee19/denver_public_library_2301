0326f236421bb54876aecef6dd886365ad3fc189
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-24 10:54:49 -0700

    Add instance variables and finish iteration 2

--- lib/book.rb
+++ lib/book.rb
@@ -3,9 +3,11 @@ class Book
               :author,
               :publication_year
   def initialize(data)
+    @author_last_name = data[:author_last_name]
+    @author_first_name = data[:author_first_name]
     @title = data[:title]
     @author = "#{data[:author_first_name]} #{data[:author_last_name]}"
-    @publication_year = data[:publication_date].split(" ")[2]
+    @publication_year = data[:publication_date].split.last
   end
 
   
