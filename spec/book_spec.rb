fde12a74be529368b1da5b641fe730902822c728
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-24 09:05:44 -0700

    Initial commit

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+require './lib/book'
+
+RSpec.describe Book do
+  let(:book) {Book.new({
+    author_first_name: "Harper", 
+    author_last_name: "Lee", 
+    title: "To Kill a Mockingbird", 
+    publication_date: "July 11, 1960"})}
+
+  it 'exists' do 
+
+
+
+
+end
